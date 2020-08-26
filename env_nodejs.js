4fee6a6532741ba4defcb0f6547b4ebcb1bf4ca7
oneconf-inet <pawat.th@inet.co.th>
2020-08-26 23:12:22 +0700

    env-node

---
+++ env_nodejs.js
@@ -0,0 +1,9 @@
+var config = {
+  API_NODE : 'https://oneconf-dev.cloudns.asia',
+  API_SOCKET_IO : 'https://oneconference-new.inet.co.th',
+  REDIRECT_ONECON : 'https://meet.one.th',
+  REDIRECT_ONECHAT : 'https://chat.one.th'
+}
+export default{
+  config
+}
